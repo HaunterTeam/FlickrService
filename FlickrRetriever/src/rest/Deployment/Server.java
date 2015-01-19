5d7039c3811cd27c760e07908bbe567007f6b838
les <nanotrippolo@gmail.com>
2015-01-19 20:46:00 +0100

    test

--- FlickrRetriever/src/rest/Deployment/Server.java
+++ FlickrRetriever/src/rest/Deployment/Server.java
@@ -8,15 +8,19 @@ import java.util.Collections;
 import java.util.Random;
 
 import com.flickr4java.flickr.Flickr;
+import com.flickr4java.flickr.FlickrException;
 import com.flickr4java.flickr.REST;
 import com.flickr4java.flickr.people.User;
 import com.flickr4java.flickr.photos.Photo;
 import com.flickr4java.flickr.photos.PhotoList;
+import com.flickr4java.flickr.photos.PhotosInterface;
+import com.flickr4java.flickr.photos.SearchParameters;
 import com.flickr4java.flickr.tags.ClusterList;
 import com.flickr4java.flickr.tags.TagsInterface;
 import com.flickr4java.flickr.test.TestInterface;
 import org.glassfish.jersey.jdkhttp.JdkHttpServerFactory;
 import org.glassfish.jersey.server.ResourceConfig;
+import rest.DAO.FlickrDao;
 
 public class Server
 {
@@ -27,6 +31,27 @@ public class Server
         System.out.println("Starting Flickr standalone HTTP server...");
         JdkHttpServerFactory.createHttpServer(BASE_URI, createApp());
         System.out.println("Server started on " + BASE_URI + "\n[kill the process to exit]");
+
+        Flickr f = FlickrDao.instance.getFlickr();
+        PhotosInterface photosInterface = f.getPhotosInterface();
+        Photo photo = null;
+
+        try {
+            SearchParameters parameters = new SearchParameters();
+            parameters.setTags(new String[]{"pizza","food"});
+            parameters.setSort(SearchParameters.RELEVANCE);
+            parameters.setText("pizza");
+
+            PhotoList<Photo> list = photosInterface.search(parameters,30,1);
+
+            Random rmd = new Random();
+            int photo_index = rmd.nextInt(list.size());
+            photo = list.get(photo_index);
+        }
+        catch (FlickrException ex){
+            System.out.println(ex.getMessage());
+        }
+        System.out.println(photo.getUrl());
 
 
     }
